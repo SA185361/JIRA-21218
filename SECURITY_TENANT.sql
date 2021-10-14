8cafcbf27f2df3a76566845bd12ec157ae4004c6
SA185361 <85183858+SA185361@users.noreply.github.com>
2021-10-14 15:58:37 +0100

    security_tenant 14 OCT

--- SECURITY_TENANT.sql
+++ SECURITY_TENANT.sql
@@ -1,37 +1,10 @@
---USE [GLOBAL]
---GO
 
---/****** Object:  Table [VISION].[SECURITY_TENANT]    Script Date: 8/30/2021 8:50:50 AM ******/
---SET ANSI_NULLS ON
---GO
-
---SET QUOTED_IDENTIFIER ON
---GO
-
---CREATE TABLE [VISION].[SECURITY_TENANT_Test](
---	[id] [numeric](19, 0) IDENTITY(1,1) NOT NULL,
---	[name] [nvarchar](32) NOT NULL,
---	[description] [nvarchar](100) NULL,
---	[status] [varchar](10) NOT NULL,
---	[shared] [bit] NOT NULL,
---	[create_date] [datetime] NULL,
---	[modified_date] [datetime] NULL,
--- CONSTRAINT [PK__SECURITY_TENANT] PRIMARY KEY CLUSTERED 
---(
---	[id] ASC
---)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
---) ON [PRIMARY]
---GO
-
-
-
-
-
-------------------------------------------------------------
 
 
 DROP Table IF Exists #SecurityT
 DROP Table IF Exists #SECURITY_TENANT1
+DROP Table if exists vision.##SECURITY_TENANT2
+
 
 CREATE TABLE #SecurityT (
  RowID int IDENTITY(1, 1),
@@ -79,8 +52,28 @@ Select
            ,[create_date]
            ,[modified_date]
 from vision.SECURITY_TENANT 
-where Tenant_id = 0
---where deleted = 'N';  --- EDIT this to Y or N based on Carissa Confirmation
+--where Tenant_id = 0
+--and deleted = 'N';  --- EDIT this to Y or N based on Carissa Confirmation
+
+
+DECLARE @sql varchar(8000);
+Declare @LINK int;
+Select @LINK = max(id) + 1 from  vision.SECURITY_TENANT 
+print @LINK
+
+SET @sql = '
+CREATE TABLE vision.##SECURITY_TENANT2(
+	[ID] [int] Identity (' + CAST(@link AS varchar(15)) + ',1) NOT NULL,
+    [name] [nvarchar](32) NOT NULL,
+	[description] [nvarchar](100) NULL,
+	[status] [varchar](10) NOT NULL,
+	[shared] [bit] NOT NULL,
+	[create_date] [datetime] NULL,
+	[modified_date] [datetime] NULL)
+
+	';
+
+EXEC (@sql);
 
 
 WHILE @RowCount <= @NumberRecords
@@ -92,7 +85,7 @@ FROM #SecurityT
 WHERE RowID = @RowCount
 
 
-Insert into Vision.SECURITY_TENANT
+Insert into vision.##SECURITY_TENANT2
 (
  [name]
            ,[description]
@@ -111,6 +104,10 @@ from  #SECURITY_TENANT1
 
 SET @RowCount = @RowCount + 1
 END
+
+
+Select * from vision.##SECURITY_TENANT2
+order by id asc
 
 DROP TABLE #SecurityT
 Drop Table #SECURITY_TENANT1 
