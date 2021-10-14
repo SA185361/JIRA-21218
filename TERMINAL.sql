0c63631104ed609eacd02e47c7420db2673b3693
SA185361 <85183858+SA185361@users.noreply.github.com>
2021-10-14 15:48:18 +0100

    Moved Child tables to child tables folder

--- TERMINAL.sql
+++ TERMINAL.sql
@@ -1,61 +1,8 @@
---USE [GLOBAL]
---GO
-
---/****** Object:  Table [VISION].[TERMINAL]    Script Date: 8/30/2021 11:27:41 AM ******/
---SET ANSI_NULLS ON
---GO
-
---SET QUOTED_IDENTIFIER ON
---GO
-
---CREATE TABLE [VISION].[TERMINAL_Test](
---	[ID] [bigint] IDENTITY(5540,1) NOT NULL,
---	[NAME] [nvarchar](30) NOT NULL,
---	[IP_ADDRESS] [nvarchar](30) NULL,
---	[ENTITY_REF_ID] [int] NULL,
---	[MAC_ADDRESS] [nvarchar](30) NULL,
---	[WORKSTATION_NAME] [nvarchar](30) NULL,
---	[UUID] [nvarchar](36) NULL,
---	[ENTITY_PAIRED] [bit] NULL,
---	[CUSTOMER_CODE] [nvarchar](50) NULL,
---	[TERMINAL_TYPE] [nvarchar](3) NULL,
---	[POLLING_MODE] [nvarchar](10) NULL,
---	[DEFAULT_POLLING_INTERVAL] [int] NULL,
---	[ACTIVE_POLLING_INTERVAL] [int] NULL,
---	[ACTIVE_POLLING_PERIOD] [int] NULL,
---	[TENANT_ID] [numeric](19, 0) NOT NULL,
---	[UA_VERSION] [varchar](50) NULL,
---	[CREATED_DATE] [datetime] NULL,
---	[UPDATED_DATE] [datetime] NULL,
---	[ATM_VERSION] [varchar](50) NULL,
--- CONSTRAINT [PK_TERMINAL_Test] PRIMARY KEY CLUSTERED 
---(
---	[ID] ASC
---)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
---) ON [PRIMARY]
---GO
-
---ALTER TABLE [VISION].[TERMINAL_Test] ADD  DEFAULT ((0)) FOR [ENTITY_PAIRED]
---GO
-
---ALTER TABLE [VISION].[TERMINAL_Test] ADD  DEFAULT ((0)) FOR [TENANT_ID]
---GO
-
---ALTER TABLE [VISION].[TERMINAL_Test]  WITH NOCHECK ADD  CONSTRAINT [FK_TERMINAL_Test_TENANT_ID] FOREIGN KEY([TENANT_ID])
---REFERENCES [VISION].[SECURITY_TENANT] ([id])
---GO
-
---ALTER TABLE [VISION].[TERMINAL_Test] CHECK CONSTRAINT [FK_TERMINAL_Test_TENANT_ID]
---GO
-
-
-
-
-------------------------------------------------------------
 
 
 DROP Table IF Exists #SecurityT
 DROP Table IF Exists #TERMINAL1
+DROP Table if exists vision.##TERMINAL2
 
 CREATE TABLE #SecurityT (
  RowID int IDENTITY(1, 1),
@@ -139,7 +86,39 @@ Select
            ,[ATM_VERSION]
 from vision.TERMINAL 
 where Tenant_id = 0
---where deleted = 'N';  --- EDIT this to Y or N based on Carissa Confirmation
+--and deleted = 'N';  --- EDIT this to Y or N based on Carissa Confirmation
+
+
+DECLARE @sql varchar(8000);
+Declare @LINK int;
+Select @LINK = max(id) + 1 from  vision.TERMINAL 
+print @LINK
+
+SET @sql = '
+CREATE TABLE vision.##TERMINAL2(
+	ID [int] Identity (' + CAST(@link AS varchar(15)) + ',1) NOT NULL,
+    [NAME] [nvarchar](30) NOT NULL,
+	[IP_ADDRESS] [nvarchar](30) NULL,
+	[ENTITY_REF_ID] [int] NULL,
+	[MAC_ADDRESS] [nvarchar](30) NULL,
+	[WORKSTATION_NAME] [nvarchar](30) NULL,
+	[UUID] [nvarchar](36) NULL,
+	[ENTITY_PAIRED] [bit] NULL,
+	[CUSTOMER_CODE] [nvarchar](50) NULL,
+	[TERMINAL_TYPE] [nvarchar](3) NULL,
+	[POLLING_MODE] [nvarchar](10) NULL,
+	[DEFAULT_POLLING_INTERVAL] [int] NULL,
+	[ACTIVE_POLLING_INTERVAL] [int] NULL,
+	[ACTIVE_POLLING_PERIOD] [int] NULL,
+	[TENANT_ID] [numeric](19, 0) NOT NULL,
+	[UA_VERSION] [varchar](50) NULL,
+	[CREATED_DATE] [datetime] NULL,
+	[UPDATED_DATE] [datetime] NULL,
+	[ATM_VERSION] [varchar](50) NULL)
+
+	';
+
+EXEC (@sql);
 
 
 WHILE @RowCount <= @NumberRecords
@@ -151,7 +130,7 @@ FROM #SecurityT
 WHERE RowID = @RowCount
 
 
-Insert into Vision.TERMINAL
+Insert into vision.##TERMINAL2
 (
   [NAME]
            ,[IP_ADDRESS]
@@ -195,11 +174,16 @@ from  #TERMINAL1
 SET @RowCount = @RowCount + 1
 END
 
+
+Select * from vision.##TERMINAL2
+
+
 DROP TABLE #SecurityT
 Drop Table #TERMINAL1 
-
+DROP Table if exists vision.##TERMINAL2
 
 GO
+
 
 -- Select * from [VISION].[TERMINAL]
 
