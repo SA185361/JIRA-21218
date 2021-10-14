caaa67b17879627d5b7ae245dfa01b5e0a6e133c
SA185361 <85183858+SA185361@users.noreply.github.com>
2021-10-14 16:53:55 +0100

    Reporting_Device no DATA to replicate 14 OCT

--- REPORTING_DEVICE.sql
+++ REPORTING_DEVICE.sql
@@ -1,51 +1,9 @@
---USE [GLOBAL]
---GO
 
-
---/****** Object:  Table [VISION].[REPORTING_DEVICE]    Script Date: 8/18/2021 7:40:55 AM ******/
---SET ANSI_NULLS ON
---GO
-
---SET QUOTED_IDENTIFIER ON
---GO
-
---CREATE TABLE [VISION].[REPORTING_DEVICE_Test](
---	[LINK] [int] NOT NULL Identity(1,1),
---	[DESCRIPTION] [nvarchar](100) NULL,
---	--[PRCNT] [smallint] NULL,
---	[TENANT_ID] [numeric](19, 0) NOT NULL,
---	--[DELETED] [char](1) NOT NULL,
---	--PREV Int Not Null,
---	[VERSION] [int] NOT NULL,
--- CONSTRAINT [pk_REPORTING_DEVICE_Test] PRIMARY KEY CLUSTERED 
---(
---	[LINK] ASC
---)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, FILLFACTOR = 90) ON [PRIMARY]
---) ON [PRIMARY]
---GO
-
---ALTER TABLE [VISION].[REPORTING_DEVICE_Test] ADD  DEFAULT ((0)) FOR [TENANT_ID]
---GO
-
---ALTER TABLE [VISION].[REPORTING_DEVICE_Test] ADD  DEFAULT ('N') FOR [DELETED]
---GO
-
---ALTER TABLE [VISION].[REPORTING_DEVICE_Test] ADD  DEFAULT ((1)) FOR [VERSION]
---GO
-
---ALTER TABLE [VISION].[REPORTING_DEVICE_Test]  WITH NOCHECK ADD  CONSTRAINT [FK_REPORTING_DEVICE_Test_TENANT_ID] FOREIGN KEY([TENANT_ID])
---REFERENCES [VISION].[SECURITY_TENANT] ([id])
---GO
-
---ALTER TABLE [VISION].[REPORTING_DEVICE_Test] NOCHECK CONSTRAINT [FK_REPORTING_DEVICE_Test_TENANT_ID]
---GO
-
-
-------------------------------------------------------------
 
 
 DROP Table IF Exists #SecurityT
 DROP Table IF Exists #REPORTING_DEVICE1
+DROP Table if exists vision.##REPORTING_DEVICE2
 
 CREATE TABLE #SecurityT (
  RowID int IDENTITY(1, 1),
@@ -85,8 +43,28 @@ TENANT_ID,
 [VERSION] 
 from vision.REPORTING_DEVICE 
 where Tenant_id = 0
---where deleted = 'N';  --- EDIT this to Y or N based on Carissa Confirmation
+-- and deleted = 'N';  --- EDIT this to Y or N based on Carissa Confirmation
 
+
+
+DECLARE @sql varchar(8000);
+Declare @LINK int;
+Select @LINK = max(link) + 1 from  vision.REPORTING_DEVICE
+print @LINK
+
+SET @sql = '
+CREATE TABLE vision.##REPORTING_DEVICE2(
+	[LINK] [int] Identity (' + CAST(@link AS varchar(15)) + ',1) NOT NULL,
+   	[DESCRIPTION] [nvarchar](100) NULL,
+	--[PRCNT] [int] NULL,
+	[TENANT_ID] [numeric](19, 0) NOT NULL,
+	--[DELETED] [char](1) NOT NULL,
+	--PREV int not null,
+	[VERSION] [int] NOT NULL)
+
+	';
+
+EXEC (@sql);
 
 WHILE @RowCount <= @NumberRecords
 
@@ -97,7 +75,7 @@ FROM #SecurityT
 WHERE RowID = @RowCount
 
 
-Insert into Vision.REPORTING_DEVICE
+Insert into vision.##REPORTING_DEVICE2
 (
  DESCRIPTION
 -- , PRCNT
