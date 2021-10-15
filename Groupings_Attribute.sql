c196f5171dccadfa99bf344138383a9b103be8e9
SA185361 <85183858+SA185361@users.noreply.github.com>
2021-10-15 15:13:03 +0100

    Groupings_Attribute 15 OCT

--- Groupings_Attribute.sql
+++ Groupings_Attribute.sql
@@ -1,63 +1,10 @@
---USE [GLOBAL]
---GO
 
---/****** Object:  Table [VISION].[Groupings_Attribute]    Script Date: 8/30/2021 8:31:47 AM ******/
---SET ANSI_NULLS ON
---GO
-
---SET QUOTED_IDENTIFIER ON
---GO
-
---CREATE TABLE [VISION].[Groupings_Attribute_Test](
---	[id] [int] IDENTITY(1,1) NOT NULL,
---	[groupings_id] [int] NOT NULL,
---	[attribute_id] [int] NULL,
---	[groupings_order] [int] NOT NULL,
---	[filters] [varchar](4000) NULL,
---	[parent_attribute_id] [int] NULL,
---	[tenant_id] [numeric](19, 0) NOT NULL,
--- CONSTRAINT [PK_Groupings_Attribute_Test] PRIMARY KEY CLUSTERED 
---(
---	[id] ASC
---)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
---) ON [PRIMARY]
---GO
-
---ALTER TABLE [VISION].[Groupings_Attribute_Test] ADD  DEFAULT ((0)) FOR [tenant_id]
---GO
-
---ALTER TABLE [VISION].[Groupings_Attribute_Test]  WITH NOCHECK ADD  CONSTRAINT [FK_Groupings_Attrib_Tenant_Id] FOREIGN KEY([tenant_id])
---REFERENCES [VISION].[SECURITY_TENANT] ([id])
---GO
-
---ALTER TABLE [VISION].[Groupings_Attribute_Test] CHECK CONSTRAINT [FK_Groupings_Attrib_Tenant_Id]
---GO
-
---ALTER TABLE [VISION].[Groupings_Attribute_Test]  WITH NOCHECK ADD  CONSTRAINT [FK_Groupings_Attribute_Test_Attribute] FOREIGN KEY([attribute_id])
---REFERENCES [VISION].[Attribute] ([id])
---GO
-
---ALTER TABLE [VISION].[Groupings_Attribute_Test] CHECK CONSTRAINT [FK_Groupings_Attribute_Test_Attribute]
---GO
-
---ALTER TABLE [VISION].[Groupings_Attribute_Test]  WITH NOCHECK ADD  CONSTRAINT [FK_Groupings_Attribute_Test_Groupings] FOREIGN KEY([groupings_id])
---REFERENCES [VISION].[Groupings] ([id])
---GO
-
---ALTER TABLE [VISION].[Groupings_Attribute_Test] CHECK CONSTRAINT [FK_Groupings_Attribute_Test_Groupings]
---GO
-
-
-
-
-
-
-
-------------------------------------------------------------
 
 
 DROP Table IF Exists #SecurityT
 DROP Table IF Exists #Groupings_Attribute1
+DROP Table if exists vision.##Groupings_Attribute2
+
 
 CREATE TABLE #SecurityT (
  RowID int IDENTITY(1, 1),
@@ -105,8 +52,27 @@ Select
            ,[tenant_id]
 from vision.Groupings_Attribute 
 where Tenant_id = 0
---where deleted = 'N';  --- EDIT this to Y or N based on Carissa Confirmation
+--and deleted = 'N';  --- EDIT this to Y or N based on Carissa Confirmation
 
+
+DECLARE @sql varchar(8000);
+Declare @LINK int;
+Select @LINK = max(ID) + 1 from  vision.Groupings_Attribute
+print @LINK
+
+SET @sql = '
+CREATE TABLE vision.##Groupings_Attribute2(
+	[ID] [int] Identity (' + CAST(@link AS varchar(15)) + ',1) NOT NULL,
+    [groupings_id] [int] NOT NULL,
+	[attribute_id] [int] NULL,
+	[groupings_order] [int] NOT NULL,
+	[filters] [varchar](4000) NULL,
+	[parent_attribute_id] [int] NULL,
+	[tenant_id] [numeric](19, 0) NOT NULL)
+
+	';
+
+EXEC (@sql);
 
 WHILE @RowCount <= @NumberRecords
 
@@ -117,7 +83,7 @@ FROM #SecurityT
 WHERE RowID = @RowCount
 
 
-Insert into Vision.Groupings_Attribute
+Insert into vision.##Groupings_Attribute2
 (
   [groupings_id]
            ,[attribute_id]
@@ -136,6 +102,9 @@ from  #Groupings_Attribute1
 
 SET @RowCount = @RowCount + 1
 END
+
+
+select * from vision.##Groupings_Attribute2
 
 DROP TABLE #SecurityT
 Drop Table #Groupings_Attribute1 
