e4908f42cb1f0f41d18e30a909194a89e7a20c2e
SA185361 <85183858+SA185361@users.noreply.github.com>
2021-10-15 17:06:01 +0100

    Entity_Type_Attribute 15 OCT

--- Entity_Type_Attribute.sql
+++ Entity_Type_Attribute.sql
@@ -1,38 +1,10 @@
---USE [GLOBAL]
---GO
-
---/****** Object:  Table [VISION].[Entity_Type_Attribute]    Script Date: 8/30/2021 7:57:10 AM ******/
---SET ANSI_NULLS ON
---GO
-
---SET QUOTED_IDENTIFIER ON
---GO
-
---CREATE TABLE [VISION].[Entity_Type_Attribute_Test](
---	[id] [int] IDENTITY(41,1) NOT NULL,
---	[entity_type_id] [int] NOT NULL,
---	[attribute_id] [int] NOT NULL,
---	[string_default_value] [nvarchar](256) NULL,
---	[integer_default_value] [int] NULL,
---	[date_default_value] [datetime] NULL,
---	[binary_default_value] [varbinary](max) NULL,
---	[boolean_default_value] [bit] NULL,
---	[tenant_id] [numeric](19, 0) NOT NULL,
--- CONSTRAINT [PK_Entity_Type_Attribute_Test] PRIMARY KEY CLUSTERED 
---(
---	[id] ASC
---)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
---) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
---GO
-
-
-
-
-------------------------------------------------------------
 
 
 DROP Table IF Exists #SecurityT
 DROP Table IF Exists #Entity_Type_Attribute_Test1
+DROP Table if exists vision.##Entity_Type_Attribute2
+
+
 
 CREATE TABLE #SecurityT (
  RowID int IDENTITY(1, 1),
@@ -61,7 +33,7 @@ Create Table #Entity_Type_Attribute_Test1
 	[date_default_value] [datetime] NULL,
 	[binary_default_value] [varbinary](max) NULL,
 	[boolean_default_value] [bit] NULL,
-	[tenant_id] [numeric](19, 0) NOT NULL,)
+	[tenant_id] [numeric](19, 0) NOT NULL)
 
 
 Insert into #Entity_Type_Attribute_Test1 
@@ -88,6 +60,29 @@ where Tenant_id = 0
 --where deleted = 'N';  --- EDIT this to Y or N based on Carissa Confirmation
 
 
+
+DECLARE @sql varchar(8000);
+Declare @LINK int;
+Select @LINK = max(ID) + 1 from  vision.Entity_Type_Attribute  
+print @LINK
+
+SET @sql = '
+CREATE TABLE vision.##Entity_Type_Attribute2(
+	[ID] [int] Identity (' + CAST(@link AS varchar(15)) + ',1) NOT NULL,
+    [entity_type_id] [int] NOT NULL,
+	[attribute_id] [int] NOT NULL,
+	[string_default_value] [nvarchar](256) NULL,
+	[integer_default_value] [int] NULL,
+	[date_default_value] [datetime] NULL,
+	[binary_default_value] [varbinary](max) NULL,
+	[boolean_default_value] [bit] NULL,
+	[tenant_id] [numeric](19, 0) NOT NULL)
+
+	';
+
+EXEC (@sql);
+
+
 WHILE @RowCount <= @NumberRecords
 
 BEGIN
@@ -97,7 +92,7 @@ FROM #SecurityT
 WHERE RowID = @RowCount
 
 
-Insert into Vision.Entity_Type_Attribute
+Insert into vision.##Entity_Type_Attribute2
 (
  		    [entity_type_id]
            ,[attribute_id]
@@ -123,6 +118,9 @@ from  #Entity_Type_Attribute_Test1
 
 SET @RowCount = @RowCount + 1
 END
+
+
+Select * from vision.##Entity_Type_Attribute2
 
 DROP TABLE #SecurityT
 Drop Table #Entity_Type_Attribute_Test1 
