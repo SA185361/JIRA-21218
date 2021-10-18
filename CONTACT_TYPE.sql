039da479fb66a2a4d2b1f18364c5dc652cb34136
SA185361 <85183858+SA185361@users.noreply.github.com>
2021-10-18 13:20:33 +0100

    CONTACT_TYPE 18 Oct

--- CONTACT_TYPE.sql
+++ CONTACT_TYPE.sql
@@ -1,50 +1,10 @@
---USE [GLOBAL]
---GO
-
---/****** Object:  Table [VISION].[CONTACT_TYPE]    Script Date: 8/18/2021 7:40:55 AM ******/
---SET ANSI_NULLS ON
---GO
-
---SET QUOTED_IDENTIFIER ON
---GO
-
---CREATE TABLE [VISION].[CONTACT_TYPE_Test](
---	[LINK] [int] NOT NULL Identity(365,1),
---	[DESCRIPTION] [nvarchar](100) NULL,
---	[PRCNT] [smallint] NULL,
---	[TENANT_ID] [numeric](19, 0) NOT NULL,
---	--[DELETED] [char](1) NOT NULL,
---	--PREV Int Not Null,
---	[VERSION] [int] NOT NULL,
--- CONSTRAINT [pk_CONTACT_TYPE_Test] PRIMARY KEY CLUSTERED 
---(
---	[LINK] ASC
---)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, FILLFACTOR = 90) ON [PRIMARY]
---) ON [PRIMARY]
---GO
-
---ALTER TABLE [VISION].[CONTACT_TYPE_Test] ADD  DEFAULT ((0)) FOR [TENANT_ID]
---GO
-
---ALTER TABLE [VISION].[CONTACT_TYPE_Test] ADD  DEFAULT ('N') FOR [DELETED]
---GO
-
---ALTER TABLE [VISION].[CONTACT_TYPE_Test] ADD  DEFAULT ((1)) FOR [VERSION]
---GO
-
---ALTER TABLE [VISION].[CONTACT_TYPE_Test]  WITH NOCHECK ADD  CONSTRAINT [FK_CONTACT_TYPE_Test_TENANT_ID] FOREIGN KEY([TENANT_ID])
---REFERENCES [VISION].[SECURITY_TENANT] ([id])
---GO
-
---ALTER TABLE [VISION].[CONTACT_TYPE_Test] NOCHECK CONSTRAINT [FK_CONTACT_TYPE_Test_TENANT_ID]
---GO
-
-
-------------------------------------------------------------
 
 
 DROP Table IF Exists #SecurityT
 DROP Table IF Exists #CONTACT_TYPE1
+DROP Table if exists vision.##CONTACT_TYPE2
+
+
 
 CREATE TABLE #SecurityT (
  RowID int IDENTITY(1, 1),
@@ -84,7 +44,29 @@ TENANT_ID,
 [VERSION] 
 from vision.CONTACT_TYPE 
 where Tenant_id = 0
---where deleted = 'N';  --- EDIT this to Y or N based on Carissa Confirmation
+-- deleted = 'N';  --- EDIT this to Y or N based on Carissa Confirmation
+
+
+
+
+DECLARE @sql varchar(8000);
+Declare @LINK int;
+Select @LINK = max(link) + 1 from vision.CONTACT_TYPE
+print @LINK
+
+SET @sql = '
+CREATE TABLE vision.##CONTACT_TYPE2(
+	[LINK] [int] Identity (' + CAST(@link AS varchar(15)) + ',1) NOT NULL,
+   	[DESCRIPTION] [nvarchar](100) NULL,
+	[PRCNT] [int] NULL,
+	[TENANT_ID] [numeric](19, 0) NOT NULL,
+	--[DELETED] [char](1) NOT NULL,
+--	PREV int not null,
+	[VERSION] [int] NOT NULL)
+
+	';
+
+EXEC (@sql);
 
 
 WHILE @RowCount <= @NumberRecords
@@ -96,7 +78,7 @@ FROM #SecurityT
 WHERE RowID = @RowCount
 
 
-Insert into Vision.CONTACT_TYPE
+Insert into vision.##CONTACT_TYPE2
 ( DESCRIPTION, PRCNT, TENANT_ID
 --, DELETED
 -- ,PREV
@@ -113,6 +95,8 @@ from  #CONTACT_TYPE1
 SET @RowCount = @RowCount + 1
 END
 
+Select * from vision.##CONTACT_TYPE2
+
 DROP TABLE #SecurityT
 Drop Table #CONTACT_TYPE1 
 
@@ -121,19 +105,3 @@ GO
 
 --  Select * from [VISION].[CONTACT_TYPE]
 
--- DBCC CHECKIDENT('vision.CONTACT_TYPE_Test')
-
--- DBCC CHECKIDENT('vision.CONTACT_TYPE_Test',reseed,365)
-
-
-
---      Select 596 * 5
---      FINAL Total: 2980
-
-
---  DELETE from Vision.CONTACT_TYPE_test
-
---  Select count(*) from [VISION].[CONTACT_TYPE_Test]
-
--- Select * from [VISION].[CONTACT_TYPE_Test]
--- where TENANT_ID =1 
