fbdda25287f6cc062c950514d674d0f9de366fd8
SA185361 <85183858+SA185361@users.noreply.github.com>
2021-10-18 13:44:12 +0100

    COMPONENT 18 Oct

--- COMPONENTsql.sql
+++ COMPONENTsql.sql
@@ -1,50 +1,9 @@
---USE [GLOBAL]
---GO
 
---/****** Object:  Table [VISION].[COMPONENT]    Script Date: 8/18/2021 7:40:55 AM ******/
---SET ANSI_NULLS ON
---GO
-
---SET QUOTED_IDENTIFIER ON
---GO
-
---CREATE TABLE [VISION].[COMPONENT_Test](
---	[LINK] [int] NOT NULL Identity(365,1),
---	[DESCRIPTION] [nvarchar](100) NULL,
---	[PRCNT] [smallint] NULL,
---	[TENANT_ID] [numeric](19, 0) NOT NULL,
---	[DELETED] [char](1) NOT NULL,
---	PREV Int Not Null,
---	[VERSION] [int] NOT NULL,
--- CONSTRAINT [pk_COMPONENT_Test] PRIMARY KEY CLUSTERED 
---(
---	[LINK] ASC
---)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, FILLFACTOR = 90) ON [PRIMARY]
---) ON [PRIMARY]
---GO
-
---ALTER TABLE [VISION].[COMPONENT_Test] ADD  DEFAULT ((0)) FOR [TENANT_ID]
---GO
-
---ALTER TABLE [VISION].[COMPONENT_Test] ADD  DEFAULT ('N') FOR [DELETED]
---GO
-
---ALTER TABLE [VISION].[COMPONENT_Test] ADD  DEFAULT ((1)) FOR [VERSION]
---GO
-
---ALTER TABLE [VISION].[COMPONENT_Test]  WITH NOCHECK ADD  CONSTRAINT [FK_COMPONENT_Test_TENANT_ID] FOREIGN KEY([TENANT_ID])
---REFERENCES [VISION].[SECURITY_TENANT] ([id])
---GO
-
---ALTER TABLE [VISION].[COMPONENT_Test] NOCHECK CONSTRAINT [FK_COMPONENT_Test_TENANT_ID]
---GO
-
-
-------------------------------------------------------------
 
 
 DROP Table IF Exists #SecurityT
 DROP Table IF Exists #COMPONENT1
+DROP Table if exists vision.##COMPONENT2
 
 CREATE TABLE #SecurityT (
  RowID int IDENTITY(1, 1),
@@ -84,7 +43,27 @@ PREV,
 [VERSION] 
 from vision.COMPONENT 
 where Tenant_id = 0
---where deleted = 'N';  --- EDIT this to Y or N based on Carissa Confirmation
+and deleted = 'N';  --- EDIT this to Y or N based on Carissa Confirmation
+
+DECLARE @sql varchar(8000);
+Declare @LINK int;
+Select @LINK = max(link) + 1 from  vision.COMPONENT 
+print @LINK
+
+SET @sql = '
+CREATE TABLE vision.##COMPONENT2(
+	[LINK] [int] Identity (' + CAST(@link AS varchar(15)) + ',1) NOT NULL,
+     	[DESCRIPTION] [nvarchar](100) NULL,
+	[PRCNT] [int] NULL,
+	[TENANT_ID] [numeric](19, 0) NOT NULL,
+	[DELETED] [char](1) NOT NULL,
+	PREV int not null,
+	[VERSION] [int] NOT NULL)
+
+	';
+
+EXEC (@sql);
+
 
 
 WHILE @RowCount <= @NumberRecords
@@ -96,7 +75,7 @@ FROM #SecurityT
 WHERE RowID = @RowCount
 
 
-Insert into Vision.COMPONENT
+Insert into vision.##COMPONENT2
 ( DESCRIPTION, PRCNT, TENANT_ID, DELETED,PREV, VERSION)
 Select 
 [DESCRIPTION],
@@ -110,6 +89,8 @@ from  #COMPONENT1
 SET @RowCount = @RowCount + 1
 END
 
+Select * from vision.##COMPONENT2
+
 DROP TABLE #SecurityT
 Drop Table #COMPONENT1 
 
@@ -117,20 +98,3 @@ Drop Table #COMPONENT1
 GO
 
 --Select * from [VISION].[COMPONENT]
-
--- DBCC CHECKIDENT('vision.COMPONENT_Test')
-
--- DBCC CHECKIDENT('vision.COMPONENT_Test',reseed,365)
-
-
-
---      Select 596 * 364
---      FINAL Total: 216944
-
-
---  DELETE from Vision.COMPONENT_test
-
---  Select count(*) from [VISION].[COMPONENT_Test]
-
---Select * from [VISION].[COMPONENT_Test]
---where TENANT_ID =1 
