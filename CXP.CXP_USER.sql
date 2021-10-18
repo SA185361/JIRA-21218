13156fb49d618c8b3b980fc9f2b4e95dc9639d96
SA185361 <85183858+SA185361@users.noreply.github.com>
2021-10-18 13:14:45 +0100

    CXP.CXP_USER 18 OCT

--- CXP.CXP_USER.sql
+++ CXP.CXP_USER.sql
@@ -1,61 +1,9 @@
---USE [GLOBAL]
---GO
 
---/****** Object:  Table [CXP].[CXP_USER]    Script Date: 8/30/2021 12:10:34 PM ******/
---SET ANSI_NULLS ON
---GO
-
---SET QUOTED_IDENTIFIER ON
---GO
-
---CREATE TABLE [CXP].[CXP_USER_TEST](
---	[ID] [bigint] IDENTITY(1512,1) NOT FOR REPLICATION NOT NULL,
---	[ORGANIZATION_NAME] [varchar](512) NOT NULL,
---	[USERNAME] [nvarchar](512) NULL,
---	[PASSWORD] [nvarchar](512) NULL,
---	[FAMILY_NAME] [nvarchar](512) NULL,
---	[GIVEN_NAME] [nvarchar](512) NULL,
---	[FULL_NAME] [nvarchar](2048) NULL,
---	[STATUS] [nvarchar](16) NOT NULL,
---	[EMAIL_ADDRESS] [nvarchar](254) NULL,
---	[TELEPHONE_NUMBER] [nvarchar](32) NULL,
---	[CITY] [nvarchar](128) NULL,
---	[COUNTRY] [nvarchar](128) NULL,
---	[POSTAL_CODE] [nvarchar](64) NULL,
---	[STATE] [nvarchar](128) NULL,
---	[STREET] [nvarchar](128) NULL,
---	[PASSWORD_LAST_SET] [datetime2](7) NOT NULL,
---	[ACCOUNT_LAST_ENABLED] [datetime2](7) NOT NULL,
---	[INVALID_LOGIN_ATTEMPTS] [int] NULL,
---	[TECHNICAL] [char](1) NOT NULL,
---	[LAST_LOGIN_SUCCESSFUL] [datetime2](7) NOT NULL,
---	[ACCOUNT_EXPIRY_DATE] [datetime2](7) NULL,
---	[row_ver] [timestamp] NOT NULL,
---	[sync_ver] [bigint] NULL,
---	[TENANT_ID] [numeric](19, 0) NULL,
--- CONSTRAINT [PK_CXP_USER_TEST] PRIMARY KEY CLUSTERED 
---(
---	[ID] ASC
---)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
---) ON [PRIMARY]
---GO
-
---ALTER TABLE [CXP].[CXP_USER_TEST] ADD  DEFAULT ((0)) FOR [INVALID_LOGIN_ATTEMPTS]
---GO
-
---ALTER TABLE [CXP].[CXP_USER_TEST] ADD  DEFAULT ('F') FOR [TECHNICAL]
---GO
-
-
-
-
-
-
-------------------------------------------------------------
 
 
 DROP Table IF Exists #SecurityT
 DROP Table IF Exists #CXP_USER1
+DROP Table if exists CXP.##CXP_USER2
 
 CREATE TABLE #SecurityT (
  RowID int IDENTITY(1, 1),
@@ -76,8 +24,7 @@ SET @NumberRecords = @@rowcount
 SET @RowCount = 1
 
 Create Table #CXP_USER1 
-(
-   	
+(   	
 	[ORGANIZATION_NAME] [varchar](512) NOT NULL,
 	[USERNAME] [nvarchar](512) NULL,
 	[PASSWORD] [nvarchar](512) NULL,
@@ -152,8 +99,45 @@ Select
            ,[TENANT_ID]
 from CXP.CXP_USER 
 where Tenant_id = 0
---where deleted = 'N';  --- EDIT this to Y or N based on Carissa Confirmation
+--and deleted = 'N';  --- EDIT this to Y or N based on Carissa Confirmation
 
+
+
+DECLARE @sql varchar(8000);
+Declare @LINK int;
+Select @LINK = max(ID) + 1 from  CXP.CXP_USER 
+print @LINK
+
+SET @sql = '
+CREATE TABLE CXP.##CXP_USER2(
+	[ID] [int] Identity (' + CAST(@link AS varchar(15)) + ',1) NOT NULL,
+    [ORGANIZATION_NAME] [varchar](512) NOT NULL,
+	[USERNAME] [nvarchar](512) NULL,
+	[PASSWORD] [nvarchar](512) NULL,
+	[FAMILY_NAME] [nvarchar](512) NULL,
+	[GIVEN_NAME] [nvarchar](512) NULL,
+	[FULL_NAME] [nvarchar](2048) NULL,
+	[STATUS] [nvarchar](16) NOT NULL,
+	[EMAIL_ADDRESS] [nvarchar](254) NULL,
+	[TELEPHONE_NUMBER] [nvarchar](32) NULL,
+	[CITY] [nvarchar](128) NULL,
+	[COUNTRY] [nvarchar](128) NULL,
+	[POSTAL_CODE] [nvarchar](64) NULL,
+	[STATE] [nvarchar](128) NULL,
+	[STREET] [nvarchar](128) NULL,
+	[PASSWORD_LAST_SET] [datetime2](7) NOT NULL,
+	[ACCOUNT_LAST_ENABLED] [datetime2](7) NOT NULL,
+	[INVALID_LOGIN_ATTEMPTS] [int] NULL,
+	[TECHNICAL] [char](1) NOT NULL,
+	[LAST_LOGIN_SUCCESSFUL] [datetime2](7) NOT NULL,
+	[ACCOUNT_EXPIRY_DATE] [datetime2](7) NULL,
+	[row_ver] [timestamp] NOT NULL,
+	[sync_ver] [bigint] NULL,
+	[TENANT_ID] [numeric](19, 0) NULL)
+
+	';
+
+EXEC (@sql);
 
 WHILE @RowCount <= @NumberRecords
 
@@ -164,7 +148,7 @@ FROM #SecurityT
 WHERE RowID = @RowCount
 
 
-Insert into CXP.CXP_USER
+Insert into CXP.##CXP_USER2
 (
  [ORGANIZATION_NAME]
            ,[USERNAME]
@@ -216,6 +200,10 @@ from  #CXP_USER1
 SET @RowCount = @RowCount + 1
 END
 
+
+Select * from CXP.##CXP_USER2
+order by ID
+
 DROP TABLE #SecurityT
 Drop Table #CXP_USER1 
 
@@ -224,19 +212,3 @@ GO
 
 -- Select * from [CXP].[CXP_USER]
 
--- DBCC CHECKIDENT('CXP.CXP_USER_Test')
-
--- DBCC CHECKIDENT('CXP.CXP_USER_Test',reseed,76)
-
-
-
---      Select 596 * 1422
---      FINAL Total: 847512
-
-
---  DELETE from CXP.CXP_USER_test
-
---  Select count(*) from [CXP].[CXP_USER_Test]
-
---Select * from [CXP].[CXP_USER_Test]
---where TENANT_ID =1 
